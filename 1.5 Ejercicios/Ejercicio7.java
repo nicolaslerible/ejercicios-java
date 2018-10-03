42ca73fdf4ae838104506ef8cb71fbf7e66efae4
Nicolás Lerible García <nikolasere@gmail.com>
2018-10-03 11:47:16 +0200

    hola

---
+++ 1.5 Ejercicios/Ejercicio7.java
@@ -0,0 +1,13 @@
+
+public class Ejercicio7 {
+	
+	public static void main (String args[]) {
+    System.out.println("    *    ");
+    System.out.println("   * *   ");
+    System.out.println("  *   *  ");
+    System.out.println(" *     * ");
+    System.out.println("*********");
+		
+	}
+}
+
